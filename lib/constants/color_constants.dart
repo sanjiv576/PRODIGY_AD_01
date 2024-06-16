39e80dc77f88f81d3f028812e07a996de68294be
sanjiv576 <shresthasanjiv576@gmail.com>
2024-06-16 11:08:32 +0545

    add constant colors

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class KColors {
+  KColors._();
+
+  static const Color highEmphasisBlueColor = Color(0xFF4B5EFC);
+  static const Color lightMediumEmphasisColor = Color(0xFFD2D3DA);
+  static const Color lightLowEmphasisColor = Color(0xFFFFFFFF);
+
+  static const Color darkMediumEmphasisColor = Color(0xFF4E505F);
+  static const Color darkLowEmphasisColor = Color(0xFF2E2F38);
+
+  // background color
+  static const Color darkBgColor = Colors.black;
+  static const Color lightBgColor = Colors.white;
+
+  // text color
+  static const Color lightTextColor = Colors.black;
+  static const Color darkTextColor = Colors.white;
+}
